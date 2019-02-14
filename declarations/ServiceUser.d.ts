28eb80b698e423b693edcaf85f83524b12b14f28
LorisC <loris@ciprin.fr>
2019-02-14 18:23:17 +0100

    Change css to make it look like a grid

--- declarations/ServiceUser.d.ts
+++ declarations/ServiceUser.d.ts
@@ -1,43 +1,14 @@
-/*
- * Copyright 2019 SpinalCom - www.spinalcom.com
- *
- *  This file is part of SpinalCore.
- *
- *  Please read all of the following terms and conditions
- *  of the Free Software license Agreement ("Agreement")
- *  carefully.
- *
- *  This Agreement is a legally binding contract between
- *  the Licensee (as defined below) and SpinalCom that
- *  sets forth the terms and conditions that govern your
- *  use of the Program. By installing and/or using the
- *  Program, you agree to abide by all the terms and
- *  conditions stated or referenced herein.
- *
- *  If you do not agree to abide by these terms and
- *  conditions, do not demonstrate your acceptance and do
- *  not install or use the Program.
- *  You should have received a copy of the license along
- *  with this file. If not, see
- *  <http://resources.spinalcom.com/licenses.pdf>.
- */
-
 import { UserInterface } from 'spinal-models-user/declarations/SpinalUser';
-
 export declare class ServiceUser {
     contextId: string;
     private context;
-  initialized: boolean;
-  private users;
+    initialized: boolean;
+    private users;
     private createContext;
     init(): void;
-
-  private findEmail;
-  private findUserWithEmailPassword;
-
-  createUser(url: any, user: UserInterface): Promise<UserInterface | string>;
-
-  getUser(url: string, email: string, password: string): Promise<UserInterface>;
-
-  addNode(userId: string, childId: string, relationName: string, relationType: string): Promise<boolean>;
+    createUser(url: any, user: UserInterface): Promise<UserInterface | string>;
+    getUser(url: string, email: string, password: string): Promise<UserInterface>;
+    addNode(userId: string, childId: string, relationName: string, relationType: string): Promise<boolean>;
+    private findEmail;
+    private findUserWithEmailPassword;
 }
--- dist/ServiceUser.js
+++ dist/ServiceUser.js
@@ -1,29 +1,5 @@
-/*
- * Copyright 2019 SpinalCom - www.spinalcom.com
- *
- *  This file is part of SpinalCore.
- *
- *  Please read all of the following terms and conditions
- *  of the Free Software license Agreement ("Agreement")
- *  carefully.
- *
- *  This Agreement is a legally binding contract between
- *  the Licensee (as defined below) and SpinalCom that
- *  sets forth the terms and conditions that govern your
- *  use of the Program. By installing and/or using the
- *  Program, you agree to abide by all the terms and
- *  conditions stated or referenced herein.
- *
- *  If you do not agree to abide by these terms and
- *  conditions, do not demonstrate your acceptance and do
- *  not install or use the Program.
- *  You should have received a copy of the license along
- *  with this file. If not, see
- *  <http://resources.spinalcom.com/licenses.pdf>.
- */
-
 "use strict";
-Object.defineProperty( exports, "__esModule", { value: true } );
+Object.defineProperty(exports, "__esModule", { value: true });
 /*
  * Copyright 2019 SpinalCom - www.spinalcom.com
  *
@@ -47,113 +23,107 @@ Object.defineProperty( exports, "__esModule", { value: true } );
  *  with this file. If not, see
  *  <http://resources.spinalcom.com/licenses.pdf>.
  */
-const spinal_env_viewer_graph_service_1 = require( "spinal-env-viewer-graph-service" );
-const Constants_1 = require( "./Constants" );
-const Errors_1 = require( "./Errors" );
+const spinal_env_viewer_graph_service_1 = require("spinal-env-viewer-graph-service");
+const Constants_1 = require("./Constants");
+const Errors_1 = require("./Errors");
 const gRoot = typeof window === 'undefined' ? global : window;
 class ServiceUser {
-  constructor() {
-    this.initialized = false;
-  }
-  
-  createContext() {
-    return spinal_env_viewer_graph_service_1.SpinalGraphService.addContext( Constants_1.SERVICE_NAME, Constants_1.SERVICE_TYPE, undefined )
-      .then( ( context ) => {
-        this.context = context;
-        this.contextId = context.info.id.get();
-      } )
-      .catch( ( e ) => {
-        console.error( e );
-        return Promise.reject( Error( Errors_1.CANNOT_CREATE_CONTEXT_INTERNAL_ERROR ) );
-      } );
-  }
-  
-  init() {
-    this.context = spinal_env_viewer_graph_service_1.SpinalGraphService.getContext( Constants_1.SERVICE_NAME );
-    this.users = new Set();
-    this.initialized = true;
-    if (typeof this.context !== 'undefined') {
-      this.contextId = this.context.info.id.get();
-      spinal_env_viewer_graph_service_1.SpinalGraphService.getChildren( this.contextId, [Constants_1.RELATION_NAME] )
-        .then( ( children ) => {
-          for (let i = 0; i < children.length; i = i + 1) {
-            this.users.add( children[i].id );
-          }
-        } )
-        .catch( ( e ) => {
-          console.error( e );
-        } );
-    } else {
-      this.createContext()
-        .catch( ( e ) => {
-          throw new Error( e );
-        } );
+    constructor() {
+        this.initialized = false;
     }
-  }
-  
-  createUser( url, user ) {
-    return this.findEmail( user.email )
-      .then( ( exist ) => {
-        if (exist) {
-          return Promise.resolve( Errors_1.USER_ALREADY_EXIST );
+    createContext() {
+        return spinal_env_viewer_graph_service_1.SpinalGraphService.addContext(Constants_1.SERVICE_NAME, Constants_1.SERVICE_TYPE, undefined)
+            .then((context) => {
+            this.context = context;
+            this.contextId = context.info.id.get();
+        })
+            .catch((e) => {
+            console.error(e);
+            return Promise.reject(Error(Errors_1.CANNOT_CREATE_CONTEXT_INTERNAL_ERROR));
+        });
+    }
+    init() {
+        this.context = spinal_env_viewer_graph_service_1.SpinalGraphService.getContext(Constants_1.SERVICE_NAME);
+        this.users = new Set();
+        this.initialized = true;
+        if (typeof this.context !== 'undefined') {
+            this.contextId = this.context.info.id.get();
+            spinal_env_viewer_graph_service_1.SpinalGraphService.getChildren(this.contextId, [Constants_1.RELATION_NAME])
+                .then((children) => {
+                for (let i = 0; i < children.length; i = i + 1) {
+                    this.users.add(children[i].id);
+                }
+            })
+                .catch((e) => {
+                console.error(e);
+            });
         }
-        const userId = spinal_env_viewer_graph_service_1.SpinalGraphService.createNode( user, undefined );
-        user.id = userId;
-        return spinal_env_viewer_graph_service_1.SpinalGraphService.addChildInContext( this.contextId, userId, this.contextId, Constants_1.RELATION_NAME, Constants_1.RELATION_TYPE ).then( () => {
-          return Promise.resolve( user );
-        } );
-      } )
-      .catch( ( e ) => {
-        return Promise.resolve( e );
-      } );
-    // @ts-ignore
-  }
-  
-  getUser( url, email, password ) {
-    // @ts-ignore
-    return this.findUserWithEmailPassword( email, password );
-  }
-  
-  addNode( userId, childId, relationName, relationType ) {
-    return spinal_env_viewer_graph_service_1.SpinalGraphService.addChild( userId, childId, relationName, relationType );
-  }
-  
-  findEmail( email ) {
-    return spinal_env_viewer_graph_service_1.SpinalGraphService.getChildren( this.contextId, [Constants_1.RELATION_NAME] )
-      .then( ( children ) => {
-        if (children.length < 0) {
-          return Promise.resolve( false );
+        else {
+            this.createContext()
+                .catch((e) => {
+                throw new Error(e);
+            });
         }
-        for (let i = 0; i < children.length; i = i + 1) {
-          if (children[i].hasOwnProperty( 'email' )
-            && children[i].email.get() === email) {
-            return Promise.resolve( true );
-          }
-        }
-        return Promise.resolve( false );
-      } );
-  }
-  
-  findUserWithEmailPassword( email, password ) {
-    return spinal_env_viewer_graph_service_1.SpinalGraphService.getChildren( this.contextId, [Constants_1.RELATION_NAME] )
-      .then( ( children ) => {
-        if (children.length < 0) {
-          return Promise.reject( Errors_1.USER_BASE_EMPTY );
-        }
-        for (let i = 0; i < children.length; i = i + 1) {
-          if (children[i].hasOwnProperty( 'email' )
-            && children[i].email.get() === email
-            && children[i].hasOwnProperty( 'password' )
-            && children[i].password.get() === password) {
-            return Promise.resolve( children[i] );
-          }
-        }
-        return Promise.resolve( Errors_1.USER_NOT_FOUND );
-      } ).catch( (( e ) => {
-        console.error( e );
-        return Promise.resolve( e );
-      }) );
-  }
+    }
+    createUser(url, user) {
+        return this.findEmail(user.email)
+            .then((exist) => {
+            if (exist) {
+                return Promise.resolve(Errors_1.USER_ALREADY_EXIST);
+            }
+            const userId = spinal_env_viewer_graph_service_1.SpinalGraphService.createNode(user, undefined);
+            user.id = userId;
+            return spinal_env_viewer_graph_service_1.SpinalGraphService.addChildInContext(this.contextId, userId, this.contextId, Constants_1.RELATION_NAME, Constants_1.RELATION_TYPE).then(() => {
+                return Promise.resolve(user);
+            });
+        })
+            .catch((e) => {
+            return Promise.resolve(e);
+        });
+        // @ts-ignore
+    }
+    getUser(url, email, password) {
+        // @ts-ignore
+        return this.findUserWithEmailPassword(email, password);
+    }
+    addNode(userId, childId, relationName, relationType) {
+        return spinal_env_viewer_graph_service_1.SpinalGraphService.addChild(userId, childId, relationName, relationType);
+    }
+    findEmail(email) {
+        return spinal_env_viewer_graph_service_1.SpinalGraphService.getChildren(this.contextId, [Constants_1.RELATION_NAME])
+            .then((children) => {
+            if (children.length < 0) {
+                return Promise.resolve(false);
+            }
+            for (let i = 0; i < children.length; i = i + 1) {
+                if (children[i].hasOwnProperty('email')
+                    && children[i].email.get() === email) {
+                    return Promise.resolve(true);
+                }
+            }
+            return Promise.resolve(false);
+        });
+    }
+    findUserWithEmailPassword(email, password) {
+        return spinal_env_viewer_graph_service_1.SpinalGraphService.getChildren(this.contextId, [Constants_1.RELATION_NAME])
+            .then((children) => {
+            if (children.length < 0) {
+                return Promise.reject(Errors_1.USER_BASE_EMPTY);
+            }
+            for (let i = 0; i < children.length; i = i + 1) {
+                if (children[i].hasOwnProperty('email')
+                    && children[i].email.get() === email
+                    && children[i].hasOwnProperty('password')
+                    && children[i].password.get() === password) {
+                    return Promise.resolve(children[i]);
+                }
+            }
+            return Promise.resolve(Errors_1.USER_NOT_FOUND);
+        }).catch(((e) => {
+            console.error(e);
+            return Promise.resolve(e);
+        }));
+    }
 }
 exports.ServiceUser = ServiceUser;
 //# sourceMappingURL=ServiceUser.js.map
